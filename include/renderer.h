6421a4b0f14bbfb05c8b4c531746e9adc4e2f9c9
Pedro Villegas <pedroargv@icloud.com>
2023-02-05 11:58:42 +0000

    added progressive rendering support

--- include/renderer.h
+++ include/renderer.h
@@ -13,7 +13,7 @@
 class Renderer
 {
 public:
-    Renderer(Shader& shader, uint ViewportWidth, uint ViewportHeight);
+    Renderer(Shader& PathTracerShader, Shader& AccumShader, Shader& FinalOutputShader, uint ViewportWidth, uint ViewportHeight);
     ~Renderer();
 
     void OnResize(uint width, uint height);
@@ -21,22 +21,28 @@ public:
     void SetViewportWidth(uint width);
     void SetViewportHeight(uint height);
 
-    inline uint GetViewportWidth() const { return m_ViewportWidth; }
-    inline uint GetViewportHeight() const { return m_ViewportHeight; }
-    inline Shader* GetShader() const { return m_Shader; }
-    inline Framebuffer GetViewportFramebuffer() const { return m_ViewportFramebuffer; }
+    uint GetViewportWidth() const { return m_ViewportWidth; }
+    uint GetViewportHeight() const { return m_ViewportHeight; }
+    Shader* GetShader() const { return m_PathTraceShader; }
+    Framebuffer GetViewportFramebuffer() const { return m_FinalOutputFBO; }
+    uint GetIterations() const { return m_SampleIterations; }
 
-    inline void SetClearColour(const glm::vec4& colour) { glClearColor(colour.r, colour.g, colour.b, colour.a); GLCall; }
-    inline void Clear() { glClear(GL_COLOR_BUFFER_BIT); GLCall; }
+    void SetClearColour(const glm::vec4& colour) { glClearColor(colour.r, colour.g, colour.b, colour.a); GLCall; }
+    void Clear() { glClear(GL_COLOR_BUFFER_BIT); GLCall; }
 
-    void Render(const Scene& scene, const Camera& camera, uint VAO, int frameCounter);
+    void Render(const Scene& scene, const Camera& camera, uint VAO);
+    void ResetSamples() { m_SampleIterations = 0; }
 
 private:
+    uint m_SampleIterations = 0;
     const Camera* m_Camera = nullptr;
     const Scene* m_Scene = nullptr;
-    Shader* m_Shader = nullptr;
+    Shader* m_PathTraceShader = nullptr;
+    Shader* m_AccumShader = nullptr;
+    Shader* m_FinalOutputShader = nullptr;
     uint m_ViewportWidth, m_ViewportHeight = 0;
     FramebufferSpec m_ViewportSpec;
-    Framebuffer m_ViewportFramebuffer;
-    Framebuffer m_AccumulationBuffer;
+    Framebuffer m_PathTraceFBO;
+    Framebuffer m_AccumulationFBO;
+    Framebuffer m_FinalOutputFBO;
 };
