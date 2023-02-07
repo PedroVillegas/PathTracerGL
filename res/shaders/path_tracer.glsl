c22af31af17191bcfb0c8c42cb6fd31c51aa1d79
Pedro Villegas <pedroargv@icloud.com>
2023-02-07 11:32:41 +0000

    Bug Fixes + Refactoring

--- res/shaders/path_tracer.glsl
+++ res/shaders/path_tracer.glsl
@@ -1,11 +1,13 @@
 #version 410 core
 
-#define FLT_MAX 3.402823466e+38
-#define PI 3.14159
+#define FLT_MAX     3.402823466e+38
+#define INFINITY    1000000.0
+#define PI          3.14159265358979323
+#define EPSILON     0.001
 
-#define LAMBERTIAN 0
-#define METAL 1
-#define DIELECTRIC 2
+#define LAMBERTIAN  0
+#define METAL       1
+#define DIELECTRIC  2
 
 out vec4 colour;
 
@@ -81,23 +83,42 @@ float Rand_01()
     return float(PCGHash()) / 4294967295.0;
 }
 
-vec3 RandomSampleUnitSphere()
+vec3 UniformSampleUnitSphere(float u1, float u2)
 {
+    vec3 dir;
+    float theta = u1 * 2 * PI;
+    float z = u2 * 2.0 - 1.0;
+    float r = sqrt(1.0 - z * z);
+    float x = r * cos(theta);
+    float y = r * sin(theta);
+    dir = vec3(x, y, z);
+
+    return dir;
+}
+
+vec3 UniformSampleUnitSphere()
+{
+    vec3 dir;
     float theta = Rand_01() * 2 * PI;
     float z = Rand_01() * 2.0 - 1.0;
     float r = sqrt(1.0 - z * z);
     float x = r * cos(theta);
     float y = r * sin(theta);
-    vec3 result = vec3(x, y, z);
-    return result;
+    dir = vec3(x, y, z);
+
+    return dir;
 }
 
-vec3 CosineHemisphereSampling()
+vec3 CosineHemisphereSampling(float u1, float u2)
 {
-    float cosine_t = sqrt((1.0 - Rand_01()));
-    float sine_t = sqrt((1.0 - cosine_t * cosine_t));
-    float phi = Rand_01() * 2 * PI;
-    return vec3(cos(phi) * sine_t, sin(phi) * sine_t, cosine_t);
+    vec3 dir;
+	float r = sqrt(u1);
+	float phi = 2.0 * PI * u2;
+	dir.x = r * cos(phi);
+	dir.y = r * sin(phi);
+	dir.z = sqrt(max(0.0, 1.0 - dir.x*dir.x - dir.y*dir.y));
+
+	return dir;
 }
 
 float SchlicksApproximation(float cosine_t, float refraction_ratio)
@@ -126,17 +147,20 @@ Ray ComputeRay(vec2 uv)
 
 Hit_record ClosestHit(Ray ray, float hit_distance, int objectIndex)
 {
+    Sphere closestSphere = objectData.Spheres[objectIndex];
+    float radius = closestSphere.position.w;
     Hit_record Hit_rec;
+    
     Hit_rec.hit_distance = hit_distance;
     Hit_rec.objectIndex = objectIndex;
     
-    Sphere closestSphere = objectData.Spheres[objectIndex];
-
-    // Calculate outside normal of sphere and intersection point in world space
-    vec3 centre = vec3(closestSphere.position.xyz);
     Hit_rec.position = ray.origin + ray.direction * hit_distance;
     Hit_rec.mat = closestSphere.mat; 
-    vec3 outward_normal = normalize(Hit_rec.position - centre);
+
+    vec3 centre = vec3(closestSphere.position.xyz);
+    vec3 pos = Hit_rec.position;
+    vec3 outward_normal = (pos - centre) / radius;
+
     Hit_rec.front_face = dot(ray.direction, outward_normal) < 0;
     Hit_rec.normal = Hit_rec.front_face ? outward_normal : -outward_normal;
 
@@ -153,31 +177,39 @@ Hit_record Miss()
 Hit_record TraceRay(Ray ray)
 {
     int closestSphereIndex = -1;
-    float hit_distance = FLT_MAX;
+    float hit_distance = INFINITY;
 
     for (int i = 0; i < objectData.sphereCount; i++)
     {        
         Sphere sphere = objectData.Spheres[i];
-        vec3 origin = ray.origin - sphere.position.xyz;
-        float radius = sphere.position.w;
 
-        // Evaluate intersections points between ray and sphere
+        float radius = sphere.position.w;
+        vec3 OC = ray.origin - sphere.position.xyz;
+        vec3 D = ray.direction;
+
+        // Evaluate intersection points between ray and sphere
         // by solving quadratic equation
-        float a = dot(ray.direction, ray.direction);
-        float half_b = dot(origin, ray.direction);
-        float c = dot(origin, origin) - radius * radius;
+        float a = dot(D, D);
+        float half_b = dot(OC, D);
+        float c = dot(OC, OC) - radius * radius;
 
         // Discriminant hit test (< 0 means no real solution)
-        float discriminant = half_b * half_b - a * c;
-        if (discriminant < 0.0)
+        float disc = half_b * half_b - a * c;
+        if (disc < 0.0)
             continue;
         
-        // Ignore furthest intersection
-        // float rootOne = (-b + sqrt(discriminant)) / (2.0 * a);
-        float closestRoot = (-half_b - sqrt(discriminant)) / a;
-        if (closestRoot > 0.0 && closestRoot < hit_distance)
+        disc = sqrt(disc);
+        float t1 = (-half_b - disc) / a;
+        if (t1 > EPSILON && t1 < hit_distance)
         {
-            hit_distance = closestRoot;
+            hit_distance = t1;
+            closestSphereIndex = i;
+        }
+
+        float t2 = (-half_b + disc) / a;
+        if (t2 > EPSILON && t2 < hit_distance)
+        {
+            hit_distance = t2;
             closestSphereIndex = i;
         }
     }
@@ -201,9 +233,11 @@ vec3 PerPixel(Ray ray)
         // If ray misses, object takes on colour of the sky
         if (Hit_rec.hit_distance < 0)
         {
-            vec3 unit_direction = normalize(ray.direction);
-            float t = 0.5*(unit_direction.y + 1.0);
+            vec3 D = normalize(ray.direction);
+            
+            float t = 0.5*(D.y + 1.0);
             vec3 sky_clr = (1.0-t)*vec3(1.0) + t*vec3(0.5, 0.7, 1.0);
+
             colour *= sky_clr;
             break;
         }
@@ -213,60 +247,87 @@ vec3 PerPixel(Ray ray)
 
         if (Hit_rec.mat.type.x == LAMBERTIAN)
         {
-            // Lambertian Scattering
-            ray.origin = Hit_rec.position + Hit_rec.normal * 0.001;
-            vec3 scattered_dir = Hit_rec.position + Hit_rec.normal + RandomSampleUnitSphere();
-            ray.direction = normalize(scattered_dir - Hit_rec.position);
-            attenuation = sphere.mat.albedo.xyz;
+            // Lambert Scattering
+            vec3 N = Hit_rec.normal;
+            vec3 P = Hit_rec.position;
+            vec3 albedo = sphere.mat.albedo.xyz;
+
+            vec3 scattered_dir;
+
+            float r1 = Rand_01();
+            float r2 = Rand_01();
+
+            vec3 UpVector = abs(N.z) < 0.999 ? vec3(0, 0, 1) : vec3(1, 0, 0);
+            vec3 TangentX = normalize(cross(UpVector, N));
+            vec3 TangentY = cross(N, TangentX);
+
+            scattered_dir = CosineHemisphereSampling(r1, r2);
+            scattered_dir = TangentX * scattered_dir.x + TangentY * scattered_dir.y + N * scattered_dir.z;
+
+            //scattered_dir = N + UniformSampleUnitSphere(r1, r2); // RT in one weekend
+
+            ray.origin = P + N * EPSILON;
+            ray.direction = normalize(scattered_dir);
+
+            attenuation = albedo;
         }
         else if (Hit_rec.mat.type.x == METAL)
         {
             // Metal Scattering
-            vec3 reflected = reflect(normalize(ray.direction), Hit_rec.normal);
-            ray.direction = reflected + Hit_rec.mat.roughness * RandomSampleUnitSphere();
-            ray.origin = Hit_rec.position + Hit_rec.normal * 0.001;
-            (dot(ray.direction, Hit_rec.normal) > 0) ? attenuation = sphere.mat.albedo.xyz : attenuation = vec3(0.0);
+            vec3 N = Hit_rec.normal;
+            vec3 P = Hit_rec.position;
+            float R = Hit_rec.mat.roughness;
+            vec3 albedo = sphere.mat.albedo.xyz;
+
+            vec3 reflected;
+
+            reflected = reflect(normalize(ray.direction), N);
+            ray.direction = reflected + R * UniformSampleUnitSphere();
+            ray.origin = P + N * EPSILON;
+
+            attenuation = (dot(ray.direction, N) > 0) ? albedo : vec3(0.0);
         }
         
         else if (Hit_rec.mat.type.x == DIELECTRIC)
         {
             // Glass Scattering
             attenuation = vec3(1.0); // Attenuation always 1 since glass doesn't absorb anything
-            vec3 n = Hit_rec.normal;
-            vec3 d = normalize(ray.direction);
+            vec3 P = Hit_rec.position;
+            vec3 N = Hit_rec.normal;
+            vec3 D = normalize(ray.direction);
             float ior = Hit_rec.mat.ior;
 
             // When the incident ray intersects object from the outside, the ray goes from air (ior = 1.0) -> dielectric thus the
             // ratio is the inverse of the objects ior, otherwise, the ratio is ior/1.0
             float refraction_ratio = Hit_rec.front_face ? (1.0 / ior) : ior;
 
-            float cosine_t = min(dot(-d, n), 1.0);
-            float sine_t = sqrt(1.0 - cosine_t * cosine_t);
+            float cosine_t = min(dot(-D, N), 1.0);
+            float sine_t = sqrt(1.0 - cosine_t*cosine_t);
 
             // When the incident ray is inside the glass and outside is air, the refraction ratio will be greater than 1.0, thus
             // there is no solution to Snell's law { sin(t_1) = (ior_1 / ior_2) * sine(t_2) } when the RHS > 1.0 because sine(t_1) 
             // can't be greater than 1.0. When no solution is available, the glass cannot refract and therefore must reflect
             bool cannot_refract = refraction_ratio * sine_t > 1.0;
-            vec3 direction;
-            vec3 position;
+            vec3 dir;
+            vec3 pos;
 
             // When looking at glass from a steep angle (near perpendicular) it becomes a mirror. This is caused by the
-            // Fresnel ('Fre-nel') effect which suggests that reflections are weak when the incident ray is at angles closer
-            // to parallel to the normal and strong at angles closer to perpendicular to the normal. The coefficient of this
+            // Fresnel ('Fre-nel') effect which states that reflections are weak when the incident ray is at angles that are
+            // more parallel to the normal and strong at angles that are more perpendicular to the normal. The coefficient of this
             // effect can be approximated via 'Schlicks Approximation'
             if (cannot_refract || SchlicksApproximation(cosine_t, refraction_ratio) > Rand_01())
             {
-                direction = reflect(d, n);
-                position = Hit_rec.position + n * 0.001;
+                dir = reflect(D, N);
+                pos = P + N * EPSILON;
             }
             else
             {
-                direction = refract(d, n, refraction_ratio);
-                position = Hit_rec.position - n * 0.001;
+                dir = refract(D, N, refraction_ratio);
+                pos = P - N * EPSILON;
             }
 
-            ray.origin = position;
-            ray.direction = direction;
+            ray.origin = pos;
+            ray.direction = dir;
         }
         
         colour *= attenuation;
@@ -288,8 +349,12 @@ void main()
 
     for (int s = 0; s < samples_per_pixel; s++)
     {
-        vec2 jitter = vec2(gl_FragCoord.x + Rand_01(), gl_FragCoord.y + Rand_01()) / u_Resolution;
+        float r1 = Rand_01();
+        float r2 = Rand_01();
+
+        vec2 jitter = vec2(gl_FragCoord.x + r1, gl_FragCoord.y + r2) / u_Resolution;
         jitter = jitter * 2.0 - 1.0;
+
         Ray r = ComputeRay(jitter);
         pixel_colour += PerPixel(r);
     }
@@ -306,5 +371,6 @@ void main()
     // Once we have the new total sum of all samples we can divide (average) by the new number of samples, resulting in 
     // the new average.
     vec4 accumulatedScaledDown = newAccumulationContribution / (u_SampleIterations + 1);
+
     colour = accumulatedScaledDown;
 }
