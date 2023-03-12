40b078356ada76f313f212220b8ac32579506155
Pedro Villegas <pedroargv@icloud.com>
2023-03-12 12:53:33 +0000

    improved struct structure and added more scenes

--- src/scene.cpp
+++ src/scene.cpp
@@ -5,61 +5,202 @@ float Rand()
     return (float)(std::rand()) / (float)(RAND_MAX);
 }
 
-void Scene::Randomize()
+void Scene::CustomScene()
 {
     std::srand(time(0));
-    spheres.clear();
 
-    enum material { LAMBERTIAN, METAL, GLASS };
-    // Sphere with large radius acts as a plane
+    float radius = 1.0f;
     Sphere ground;
-    material ground_mat = LAMBERTIAN;
-    ground.position = glm::vec4(0.0f, -1000.5f, 0.0f, 1000.0f);
-    ground.mat.type.x = ground_mat;
-    ground.mat.albedo = glm::vec4(0.4f, 0.4f, 0.4f, 1.0f);
+    ground.position = glm::vec3(0.0f, -1000.0f - 2.0f * radius, 0.0f);
+    ground.radius = 1000.0f;
+    ground.mat.albedo = glm::vec3(0.8f, 0.8f, 0.8f);
+    ground.mat.specularChance = 0.0f;
     spheres.push_back(ground);
 
-    // float radius = 1.0;
+    Sphere light;
+    light.position = glm::vec3(0.0f, 2.0f * radius * 1.5f, -2.0f);
+    light.radius = radius;
+    light.mat.emissive = glm::vec3(0.3f, 0.5f, 0.8f);
+    light.mat.emissiveStrength = 300.0f;
+    spheres.push_back(light);
 
-    // Generate 64 random spheres
-    for (int a = -3; a < 3; a++) 
+    float secondLayerHeight = 2.0f * radius * 3.0f;
+    int side = 6;
+    float gap = 0.25f;
+
+    float length = (radius * 2.0f) * (float)(side) + gap * (float)(side);
+    float offset = length / 2.0f;
+    float delta = length / (float)(side - 1);
+    
+    // Row of opaque pink
+    for (int i = 0; i < side; i++)
     {
-        for (int b = -3; b < 3; b++) 
+        float inc = (float)(i) / (float)(side-1);
+        Sphere sphere;
+        sphere.position = glm::vec3((float)i * delta - offset, 0.0f, 2.0f * radius);
+        sphere.radius = radius;
+        sphere.mat.albedo = glm::vec3(0.9f, 0.25f, 0.25f);
+        sphere.mat.specularChance = 0.02f;
+        sphere.mat.specularRoughness = 0.0f; 
+        sphere.mat.refractionChance = 0.0f;
+        sphere.mat.refractionRoughness = 0.0f;
+        sphere.mat.ior = 1.0f + inc;
+        spheres.push_back(sphere);
+    }
+
+    // Row of matte purple
+    for (int i = 0; i < side; i++)
+    {
+        float albInc = (float)(i+1) / (float)side;
+        float inc = (float)(i) / (float)(side-1);
+        Sphere sphere;
+        sphere.position = glm::vec3((float)i * delta - offset, secondLayerHeight, 2.0f * radius);
+        sphere.radius = radius;
+        sphere.mat.albedo = glm::vec3(0.82f, 0.6f, 0.95) * albInc;
+        sphere.mat.specularChance = 0.16f;
+        sphere.mat.specularRoughness = 1.0f - inc; 
+        sphere.mat.refractionChance = 0.0f;
+        sphere.mat.refractionRoughness = 0.0f;
+        sphere.mat.ior = 1.0f;
+        spheres.push_back(sphere);
+    }
+    
+
+    // Row of glass with increasing absorption
+    for (int i = 0; i < side; i++)
+    {
+        float inc = (float)(i) / (float)(side-1);
+        Sphere sphere;
+        sphere.position = glm::vec3((float)i * delta - offset, 0.0f, -2.0f * radius);
+        sphere.radius = radius;
+        sphere.mat.albedo = glm::vec3(0.0f, 0.0f, 0.0f);
+        sphere.mat.absorption = glm::vec3(1.0f, 2.0f, 3.0f) * inc;
+        sphere.mat.refractionChance = 0.96f;
+        sphere.mat.refractionRoughness = 0.0f;
+        sphere.mat.ior = 1.1f;
+        sphere.mat.specularChance = 0.04f;
+        sphere.mat.specularRoughness = 0.0f;
+        spheres.push_back(sphere);
+    }
+
+    // Row of glass with increasing roughness
+    for (int i = 0; i < side; i++)
+    {
+        float inc = (float)(i) / (float)(side-1);
+        Sphere sphere;
+        sphere.position = glm::vec3((float)i * delta - offset, secondLayerHeight, -2.0f * radius);
+        sphere.radius = radius;
+        sphere.mat.albedo = glm::vec3(0.0f, 0.0f, 0.0f);
+        sphere.mat.absorption = glm::vec3(0.0f, 0.0f, 0.0f);
+        sphere.mat.refractionChance = 1.0f;
+        sphere.mat.refractionRoughness = inc;
+        sphere.mat.ior = 1.1f;
+        sphere.mat.specularChance = 0.02f;
+        sphere.mat.specularRoughness = inc;
+        spheres.push_back(sphere);
+    }
+}
+
+void Scene::RTIW()
+{
+    Sphere ground_sphere;
+    ground_sphere.position = glm::vec3(0.0f, -1001.0f, 0.0f);
+    ground_sphere.radius = 1000.0f;
+    ground_sphere.mat.albedo = glm::vec3(0.8f, 0.8f, 0.0f);
+    spheres.push_back(ground_sphere);
+    
+    Sphere centre_sphere;
+    centre_sphere.mat.albedo = glm::vec3(0.1f, 0.2f, 0.5f);
+    centre_sphere.mat.specularChance = 0.16f;
+    centre_sphere.mat.specularRoughness = 0.1f;
+    centre_sphere.mat.ior = 1.2f;
+    spheres.push_back(centre_sphere);
+
+    Sphere left_sphere;
+    left_sphere.position = glm::vec3(-2.0f, 0.0f, 0.0f);
+    left_sphere.mat.refractionChance = 0.98f;
+    left_sphere.mat.refractionRoughness = 0.1f;
+    left_sphere.mat.ior = 1.2f;
+    left_sphere.mat.specularChance = 0.02f;
+    left_sphere.mat.specularRoughness = 0.1f;
+    left_sphere.mat.absorption = glm::vec3(2.0f, 1.0f, 0.5f);
+    spheres.push_back(left_sphere);
+
+    Sphere right_sphere;
+    right_sphere.position = glm::vec3(2.0f, 0.0f, 0.0f);
+    right_sphere.mat.albedo = { 0.8f, 0.6f, 0.2f };
+    right_sphere.mat.refractionChance = 0.98f;
+    right_sphere.mat.refractionRoughness = 0.5f;
+    right_sphere.mat.ior = 1.2f;
+    right_sphere.mat.specularChance = 0.02f;
+    right_sphere.mat.specularRoughness = 0.5f;
+    right_sphere.mat.absorption = glm::vec3(2.0f, 1.0f, 0.5f);
+    spheres.push_back(right_sphere);
+}
+
+void Scene::RandomizeBRDF()
+{
+    std::srand(time(0));
+
+    // Sphere with large radius acts as a plane
+    Sphere ground;
+    ground.position = glm::vec3(0.0f, -1001.0f, 0.0f);
+    ground.radius = 1000.0f;
+    ground.mat.albedo = glm::vec3(0.8f);
+    spheres.push_back(ground);
+
+    Sphere light;
+    light.position = glm::vec3(0.0f, 10.0f, 0.0f);
+    light.radius = 0.5f;
+    light.mat.emissive = glm::vec3(1.0f);
+    light.mat.emissiveStrength = 100.0f;
+    //spheres.push_back(light);
+
+    float radius = 1.0;
+
+    for (int a = -3; a < 2; a++) 
+    {
+        for (int b = -3; b < 2; b++) 
         {
             float mat_probability = Rand();
-            //glm::vec4 pos = glm::vec4(a + 6 * Rand(), 6 * Rand(), b + 6 * Rand(), radius);
-            glm::vec4 pos = glm::vec4(a, 6 * Rand(), b, 0.3 + 0.5 * Rand());
-
-            //if (glm::length(glm::vec3(pos.x, pos.y, pos.z) - glm::vec3(4, 0.2, 0)) > 0.9) 
+            glm::vec3 pos = glm::vec3(5 * a, 7 * Rand() + 1, 5 * b);
             {
                 Sphere sphere;
 
-                if (mat_probability < 0.4) 
+                if (mat_probability < 0.2f) 
                 {
-                    // diffuse
-                    material s_material = LAMBERTIAN;
+                    // Emissive
+                    glm::vec3 albedo = glm::vec3(Rand()*0.5+0.5, Rand()*0.5+0.5, Rand()*0.5+0.5);
                     sphere.position = pos;
-                    sphere.mat.type.x = s_material;
-                    sphere.mat.albedo = glm::vec4(Rand(), Rand(), Rand(), 1.0f);
+                    sphere.radius = radius * 0.2f;
+                    sphere.mat.albedo = albedo;
+                    sphere.mat.emissive = albedo;
+                    sphere.mat.emissiveStrength = 1000.0f * Rand();
                     spheres.push_back(sphere);
                 } 
-                else if (mat_probability < 0.8) 
+                else if (mat_probability < 0.2f + 0.4f) 
                 {
-                    // metal
-                    material s_material = METAL;
+                    // Specular
                     sphere.position = pos;
-                    sphere.mat.type.x = s_material;
-                    sphere.mat.albedo = glm::vec4(Rand(), Rand(), Rand(), 1.0f);
-                    sphere.mat.roughness = Rand();
+                    sphere.radius = radius;
+                    sphere.mat.albedo = glm::vec3(Rand()*0.5+0.3, Rand()*0.5+0.2, Rand()*0.5+0.2);
+                    sphere.mat.specularChance = Rand() * 0.16f;
+                    sphere.mat.specularRoughness = Rand() * 0.2f;
+                    sphere.mat.ior = 1.0f + Rand();
                     spheres.push_back(sphere);
                 } 
                 else 
                 {
-                    // glass
-                    material s_material = GLASS;
+                    // Glass
+                    float r = Rand() * 0.0f;
                     sphere.position = pos;
-                    sphere.mat.type.x = s_material;
+                    sphere.radius = radius;
                     sphere.mat.ior = 1.55f;
+                    sphere.mat.refractionChance = 0.98f;
+                    sphere.mat.refractionRoughness = r;
+                    sphere.mat.absorption = glm::vec3(Rand(), Rand(), Rand());
+                    sphere.mat.specularChance = 0.02f;
+                    sphere.mat.specularRoughness = r;
                     spheres.push_back(sphere);
                 }
             }
--- src/scene.h
+++ src/scene.h
@@ -7,14 +7,24 @@
 
 struct alignas(16) Material
 {
-    glm::ivec4 type = { 0, 0, 0, 0 };
-    glm::vec4 albedo { 1.0f };
-    float roughness = 1.0f;
+    // Note diffuse chance = 1.0f so specularChance + refractionChance = 1.0f
+    glm::vec3 albedo { 1.0f };
     float ior = 1.0f;
+    
+    glm::vec3 emissive { 0.0f };
+    float emissiveStrength = 0.0f;
+
+    glm::vec3 absorption { 0.0f };
+    float refractionChance = 0.0f;
+    float refractionRoughness = 0.0f;
+
+    float specularChance = 0.0f;
+    float specularRoughness = 0.0f;
 };
 struct alignas(16) Sphere
 {
-    glm::vec4 position { 0.0f, 0.0f, 0.0f, 1.0f };
+    glm::vec3 position { 0.0f, 0.0f, 0.0f };
+    float radius = 1.0f;
 
     Material mat;
 };
@@ -23,7 +33,11 @@ struct Scene
 {
     int maxRayDepth = 16;
     int samplesPerPixel = 1;
+    char day = 1;
+    glm::vec3 lightDirection { 0.0f, 0.0f, 0.0f };
     std::vector<Sphere> spheres;
 
-    void Randomize();
+    void CustomScene();
+    void RTIW();
+    void RandomizeBRDF();
 };
