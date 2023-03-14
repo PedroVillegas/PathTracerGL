355ebe0d188fbca0323171f9ca83a29d8c3ccb0e
Pedro Villegas <pedroargv@icloud.com>
2023-03-14 23:38:31 +0000

    added object labels for better scene management

--- src/scene.cpp
+++ src/scene.cpp
@@ -5,23 +5,70 @@ float Rand()
     return (float)(std::rand()) / (float)(RAND_MAX);
 }
 
+void Scene::CircleScene()
+{
+    std::srand(time(0));
+
+    float radius = 1.0f;
+    Sphere ground;
+    ground.label = "Ground";
+    GPUSphere& s_ground = ground.sphere;
+    s_ground.position = glm::vec3(0.0f, -1000.0f - 2.0f * radius, 0.0f);
+    s_ground.radius = 1000.0f;
+    s_ground.mat.albedo = glm::vec3(0.8f, 0.8f, 0.8f);
+    s_ground.mat.specularChance = 0.0f;
+    spheres.push_back(ground);
+
+    Sphere light;
+    light.label = "Light";
+    GPUSphere& s_light = light.sphere;
+    s_light.position = glm::vec3(0.0f, 2.0f * radius * 1.5f, 0.0f);
+    s_light.radius = radius;
+    s_light.mat.emissive = glm::vec3(0.3f, 0.5f, 0.8f);
+    s_light.mat.emissiveStrength = 50.0f;
+    spheres.push_back(light);
+
+    int total = 16;
+    float circleRadius = 8.0f;
+    for (int i = 0; i < total; i++)
+    {
+        float offset = (2.0f * M_PI) * ((float)i / (float)total);
+        float inc = (float)i / (float)(total - 1);
+        Sphere sp;
+        sp.label = "Sphere" + std::to_string(i);
+        GPUSphere& s = sp.sphere;
+        s.position = circleRadius * glm::vec3(glm::cos(offset), 0.0f, glm::sin(offset));
+        s.radius = radius;
+        s.mat.absorption = glm::vec3(1.0f, 2.0f, 3.0f) * inc;
+        s.mat.refractionChance = 0.98f;
+        s.mat.roughness = 0.0f;
+        s.mat.ior = 1.55f;
+        s.mat.specularChance = 0.02f;
+        spheres.push_back(sp);
+    }
+}
+
 void Scene::CustomScene()
 {
     std::srand(time(0));
 
     float radius = 1.0f;
     Sphere ground;
-    ground.position = glm::vec3(0.0f, -1000.0f - 2.0f * radius, 0.0f);
-    ground.radius = 1000.0f;
-    ground.mat.albedo = glm::vec3(0.8f, 0.8f, 0.8f);
-    ground.mat.specularChance = 0.0f;
+    ground.label = "Ground";
+    GPUSphere& s_ground = ground.sphere;
+    s_ground.position = glm::vec3(0.0f, -1000.0f - 2.0f * radius, 0.0f);
+    s_ground.radius = 1000.0f;
+    s_ground.mat.albedo = glm::vec3(0.8f, 0.8f, 0.8f);
+    s_ground.mat.specularChance = 0.0f;
     spheres.push_back(ground);
 
     Sphere light;
-    light.position = glm::vec3(0.0f, 2.0f * radius * 1.5f, -2.0f);
-    light.radius = radius;
-    light.mat.emissive = glm::vec3(0.3f, 0.5f, 0.8f);
-    light.mat.emissiveStrength = 300.0f;
+    light.label = "Light";
+    GPUSphere& s_light = light.sphere;
+    s_light.position = glm::vec3(0.0f, 2.0f * radius * 1.5f, -2.0f);
+    s_light.radius = radius;
+    s_light.mat.emissive = glm::vec3(0.3f, 0.5f, 0.8f);
+    s_light.mat.emissiveStrength = 50.0f;
     spheres.push_back(light);
 
     float secondLayerHeight = 2.0f * radius * 3.0f;
@@ -37,14 +84,15 @@ void Scene::CustomScene()
     {
         float inc = (float)(i) / (float)(side-1);
         Sphere sphere;
-        sphere.position = glm::vec3((float)i * delta - offset, 0.0f, 2.0f * radius);
-        sphere.radius = radius;
-        sphere.mat.albedo = glm::vec3(0.9f, 0.25f, 0.25f);
-        sphere.mat.specularChance = 0.02f;
-        sphere.mat.specularRoughness = 0.0f; 
-        sphere.mat.refractionChance = 0.0f;
-        sphere.mat.refractionRoughness = 0.0f;
-        sphere.mat.ior = 1.0f + inc;
+        sphere.label = "Pink Sphere " + std::to_string(i);
+        GPUSphere& s = sphere.sphere;
+        s.position = glm::vec3((float)i * delta - offset, 0.0f, 2.0f * radius);
+        s.radius = radius;
+        s.mat.albedo = glm::vec3(0.9f, 0.25f, 0.25f);
+        s.mat.specularChance = 0.06f;
+        s.mat.refractionChance = 0.0f;
+        s.mat.roughness = 0.0f;
+        s.mat.ior = 1.0f + inc;
         spheres.push_back(sphere);
     }
 
@@ -54,14 +102,15 @@ void Scene::CustomScene()
         float albInc = (float)(i+1) / (float)side;
         float inc = (float)(i) / (float)(side-1);
         Sphere sphere;
-        sphere.position = glm::vec3((float)i * delta - offset, secondLayerHeight, 2.0f * radius);
-        sphere.radius = radius;
-        sphere.mat.albedo = glm::vec3(0.82f, 0.6f, 0.95) * albInc;
-        sphere.mat.specularChance = 0.16f;
-        sphere.mat.specularRoughness = 1.0f - inc; 
-        sphere.mat.refractionChance = 0.0f;
-        sphere.mat.refractionRoughness = 0.0f;
-        sphere.mat.ior = 1.0f;
+        sphere.label = "Purple Sphere " + std::to_string(i);
+        GPUSphere& s = sphere.sphere;
+        s.position = glm::vec3((float)i * delta - offset, secondLayerHeight, 2.0f * radius);
+        s.radius = radius;
+        s.mat.albedo = glm::vec3(0.82f, 0.6f, 0.95) * albInc;
+        s.mat.specularChance = 0.16f;
+        s.mat.roughness = 1.0f - inc; 
+        s.mat.refractionChance = 0.0f;
+        s.mat.ior = 1.0f;
         spheres.push_back(sphere);
     }
     
@@ -71,15 +120,16 @@ void Scene::CustomScene()
     {
         float inc = (float)(i) / (float)(side-1);
         Sphere sphere;
-        sphere.position = glm::vec3((float)i * delta - offset, 0.0f, -2.0f * radius);
-        sphere.radius = radius;
-        sphere.mat.albedo = glm::vec3(0.0f, 0.0f, 0.0f);
-        sphere.mat.absorption = glm::vec3(1.0f, 2.0f, 3.0f) * inc;
-        sphere.mat.refractionChance = 0.96f;
-        sphere.mat.refractionRoughness = 0.0f;
-        sphere.mat.ior = 1.1f;
-        sphere.mat.specularChance = 0.04f;
-        sphere.mat.specularRoughness = 0.0f;
+        sphere.label = "Absorbed Glass " + std::to_string(i);
+        GPUSphere& s = sphere.sphere; 
+        s.position = glm::vec3((float)i * delta - offset, 0.0f, -2.0f * radius);
+        s.radius = radius;
+        s.mat.albedo = glm::vec3(1.0f, 1.0f, 1.0f);
+        s.mat.absorption = glm::vec3(1.0f, 2.0f, 3.0f) * inc;
+        s.mat.refractionChance = 0.9f;
+        s.mat.ior = 1.55f;
+        s.mat.specularChance = 0.1f;
+        s.mat.roughness = 0.0f;
         spheres.push_back(sphere);
     }
 
@@ -88,15 +138,16 @@ void Scene::CustomScene()
     {
         float inc = (float)(i) / (float)(side-1);
         Sphere sphere;
-        sphere.position = glm::vec3((float)i * delta - offset, secondLayerHeight, -2.0f * radius);
-        sphere.radius = radius;
-        sphere.mat.albedo = glm::vec3(0.0f, 0.0f, 0.0f);
-        sphere.mat.absorption = glm::vec3(0.0f, 0.0f, 0.0f);
-        sphere.mat.refractionChance = 1.0f;
-        sphere.mat.refractionRoughness = inc;
-        sphere.mat.ior = 1.1f;
-        sphere.mat.specularChance = 0.02f;
-        sphere.mat.specularRoughness = inc;
+        sphere.label = "Rough Glass " + std::to_string(i);
+        GPUSphere& s = sphere.sphere;
+        s.position = glm::vec3((float)i * delta - offset, secondLayerHeight, -2.0f * radius);
+        s.radius = radius;
+        s.mat.albedo = glm::vec3(1.0f, 1.0f, 1.0f);
+        s.mat.absorption = glm::vec3(0.0f, 0.0f, 0.0f);
+        s.mat.refractionChance = 0.98f;
+        s.mat.roughness = inc;
+        s.mat.ior = 1.55f;
+        s.mat.specularChance = 0.02f;
         spheres.push_back(sphere);
     }
 }
@@ -104,37 +155,55 @@ void Scene::CustomScene()
 void Scene::RTIW()
 {
     Sphere ground_sphere;
-    ground_sphere.position = glm::vec3(0.0f, -1001.0f, 0.0f);
-    ground_sphere.radius = 1000.0f;
-    ground_sphere.mat.albedo = glm::vec3(0.8f, 0.8f, 0.0f);
+    ground_sphere.label = "Ground";
+    GPUSphere& s_ground = ground_sphere.sphere;
+    s_ground.position = glm::vec3(0.0f, -1001.0f, 0.0f);
+    s_ground.radius = 1000.0f;
+    s_ground.mat.albedo = glm::vec3(0.8f, 0.8f, 0.0f);
     spheres.push_back(ground_sphere);
     
+    Sphere light;
+    light.label = "Light";
+    GPUSphere& l = light.sphere;
+    l.position = glm::vec3(0.0f, 4.0f, 0.0f);
+    l.radius = 1.0f;
+    l.mat.emissive = glm::vec3(0.1f, 0.2f, 0.5f);
+    l.mat.emissiveStrength = 10.0f;
+    spheres.push_back(light);
+
     Sphere centre_sphere;
-    centre_sphere.mat.albedo = glm::vec3(0.1f, 0.2f, 0.5f);
-    centre_sphere.mat.specularChance = 0.16f;
-    centre_sphere.mat.specularRoughness = 0.1f;
-    centre_sphere.mat.ior = 1.2f;
+    centre_sphere.label = "Centre";
+    GPUSphere& s_centre = centre_sphere.sphere;
+    s_centre.mat.albedo = glm::vec3(0.1f, 0.2f, 0.5f);
+    s_centre.mat.specularTint = glm::vec3(1.0f);
+    s_centre.mat.specularChance = 0.16f;
+    s_centre.mat.roughness = 0.02f;
+    s_centre.mat.ior = 1.3f;
     spheres.push_back(centre_sphere);
 
     Sphere left_sphere;
-    left_sphere.position = glm::vec3(-2.0f, 0.0f, 0.0f);
-    left_sphere.mat.refractionChance = 0.98f;
-    left_sphere.mat.refractionRoughness = 0.1f;
-    left_sphere.mat.ior = 1.2f;
-    left_sphere.mat.specularChance = 0.02f;
-    left_sphere.mat.specularRoughness = 0.1f;
-    left_sphere.mat.absorption = glm::vec3(2.0f, 1.0f, 0.5f);
+    left_sphere.label = "Left";
+    GPUSphere& s_left = left_sphere.sphere;
+    s_left.position = glm::vec3(-2.0f, 0.0f, 0.0f);
+    s_left.mat.refractionChance = 0.98f;
+    s_left.mat.roughness = 0.02f;
+    s_left.mat.ior = 1.55f;
+    s_left.mat.specularTint = glm::vec3(1.0f);
+    s_left.mat.specularChance = 0.02f;
+    s_left.mat.absorption = glm::vec3(2.0f, 1.0f, 0.5f);
     spheres.push_back(left_sphere);
 
     Sphere right_sphere;
-    right_sphere.position = glm::vec3(2.0f, 0.0f, 0.0f);
-    right_sphere.mat.albedo = { 0.8f, 0.6f, 0.2f };
-    right_sphere.mat.refractionChance = 0.98f;
-    right_sphere.mat.refractionRoughness = 0.5f;
-    right_sphere.mat.ior = 1.2f;
-    right_sphere.mat.specularChance = 0.02f;
-    right_sphere.mat.specularRoughness = 0.5f;
-    right_sphere.mat.absorption = glm::vec3(2.0f, 1.0f, 0.5f);
+    right_sphere.label = "Right";
+    GPUSphere& s_right = right_sphere.sphere;
+    s_right.position = glm::vec3(2.0f, 0.0f, 0.0f);
+    s_right.mat.albedo = { 0.8f, 0.6f, 0.2f };
+    s_right.mat.refractionChance = 0.98f;
+    s_right.mat.ior = 1.55f;
+    s_right.mat.specularTint = glm::vec3(1.0f);
+    s_right.mat.specularChance = 0.02f;
+    s_right.mat.roughness = 0.02f;
+    s_right.mat.absorption = glm::vec3(2.0f, 1.0f, 0.5f);
     spheres.push_back(right_sphere);
 }
 
@@ -144,16 +213,20 @@ void Scene::RandomizeBRDF()
 
     // Sphere with large radius acts as a plane
     Sphere ground;
-    ground.position = glm::vec3(0.0f, -1001.0f, 0.0f);
-    ground.radius = 1000.0f;
-    ground.mat.albedo = glm::vec3(0.8f);
+    ground.label = "Ground";
+    GPUSphere& s_ground = ground.sphere;
+    s_ground.position = glm::vec3(0.0f, -1001.0f, 0.0f);
+    s_ground.radius = 1000.0f;
+    s_ground.mat.albedo = glm::vec3(0.8f);
     spheres.push_back(ground);
 
     Sphere light;
-    light.position = glm::vec3(0.0f, 10.0f, 0.0f);
-    light.radius = 0.5f;
-    light.mat.emissive = glm::vec3(1.0f);
-    light.mat.emissiveStrength = 100.0f;
+    light.label = "Light";
+    GPUSphere& l = light.sphere;
+    l.position = glm::vec3(0.0f, 10.0f, 0.0f);
+    l.radius = 0.5f;
+    l.mat.emissive = glm::vec3(1.0f);
+    l.mat.emissiveStrength = 100.0f;
     //spheres.push_back(light);
 
     float radius = 1.0;
@@ -166,41 +239,43 @@ void Scene::RandomizeBRDF()
             glm::vec3 pos = glm::vec3(5 * a, 7 * Rand() + 1, 5 * b);
             {
                 Sphere sphere;
+                GPUSphere& s = sphere.sphere;
 
                 if (mat_probability < 0.2f) 
                 {
                     // Emissive
+                    sphere.label = "Light" + std::to_string(std::abs(a * b));
                     glm::vec3 albedo = glm::vec3(Rand()*0.5+0.5, Rand()*0.5+0.5, Rand()*0.5+0.5);
-                    sphere.position = pos;
-                    sphere.radius = radius * 0.2f;
-                    sphere.mat.albedo = albedo;
-                    sphere.mat.emissive = albedo;
-                    sphere.mat.emissiveStrength = 1000.0f * Rand();
+                    s.position = pos;
+                    s.radius = radius;
+                    s.mat.emissive = albedo;
+                    s.mat.emissiveStrength = 50.0f;
                     spheres.push_back(sphere);
                 } 
                 else if (mat_probability < 0.2f + 0.4f) 
                 {
                     // Specular
-                    sphere.position = pos;
-                    sphere.radius = radius;
-                    sphere.mat.albedo = glm::vec3(Rand()*0.5+0.3, Rand()*0.5+0.2, Rand()*0.5+0.2);
-                    sphere.mat.specularChance = Rand() * 0.16f;
-                    sphere.mat.specularRoughness = Rand() * 0.2f;
-                    sphere.mat.ior = 1.0f + Rand();
+                    sphere.label = "Specular" + std::to_string(std::abs(a * b));
+                    s.position = pos;
+                    s.radius = radius;
+                    s.mat.albedo = glm::vec3(Rand()*0.5+0.3, Rand()*0.5+0.2, Rand()*0.5+0.2);
+                    s.mat.specularChance = Rand() * 0.16f;
+                    s.mat.roughness = Rand() * 0.2f;
+                    s.mat.ior = 1.0f + Rand();
                     spheres.push_back(sphere);
                 } 
                 else 
                 {
                     // Glass
+                    sphere.label = "Glass" + std::to_string(std::abs(a * b));
                     float r = Rand() * 0.0f;
-                    sphere.position = pos;
-                    sphere.radius = radius;
-                    sphere.mat.ior = 1.55f;
-                    sphere.mat.refractionChance = 0.98f;
-                    sphere.mat.refractionRoughness = r;
-                    sphere.mat.absorption = glm::vec3(Rand(), Rand(), Rand());
-                    sphere.mat.specularChance = 0.02f;
-                    sphere.mat.specularRoughness = r;
+                    s.position = pos;
+                    s.radius = radius;
+                    s.mat.ior = 1.55f;
+                    s.mat.refractionChance = 0.98f;
+                    s.mat.absorption = glm::vec3(Rand() * 5.0f, Rand() * 5.0f, Rand() * 5.0f);
+                    s.mat.specularChance = 0.02f;
+                    s.mat.roughness = r;
                     spheres.push_back(sphere);
                 }
             }
--- src/scene.h
+++ src/scene.h
@@ -4,8 +4,9 @@
 #include <glm/glm.hpp>
 #include <vector>
 #include <iostream>
+#include <string>
 
-struct alignas(16) Material
+struct alignas(16) Material // 65 bytes (aligned as vec4s)
 {
     // Note diffuse chance = 1.0f so specularChance + refractionChance = 1.0f
     glm::vec3 albedo { 1.0f };
@@ -16,17 +17,24 @@ struct alignas(16) Material
 
     glm::vec3 absorption { 0.0f };
     float refractionChance = 0.0f;
-    float refractionRoughness = 0.0f;
 
+    glm::vec3 specularTint { 1.0f };
     float specularChance = 0.0f;
-    float specularRoughness = 0.0f;
+    float roughness = 0.0f;
 };
-struct alignas(16) Sphere
+
+struct alignas(16) GPUSphere // 81 bytes naturally aligned
 {
     glm::vec3 position { 0.0f, 0.0f, 0.0f };
     float radius = 1.0f;
 
-    Material mat;
+    Material mat; // 65 bytes
+};
+
+struct Sphere
+{
+    std::string label; 
+    GPUSphere sphere;
 };
 
 struct Scene
@@ -37,6 +45,7 @@ struct Scene
     glm::vec3 lightDirection { 0.0f, 0.0f, 0.0f };
     std::vector<Sphere> spheres;
 
+    void CircleScene();
     void CustomScene();
     void RTIW();
     void RandomizeBRDF();
