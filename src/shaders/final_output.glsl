2b291d5908e1e8613dbdd896fef7030959b0e8dd
Pedro Villegas <pedroargv@icloud.com>
2023-04-02 21:44:59 +0100

    small changes

--- src/shaders/final_output.glsl
+++ src/shaders/final_output.glsl
@@ -8,6 +8,6 @@ uniform sampler2D u_PT_Texture;
 void main()
 {
     vec2 uv = (gl_FragCoord.xy / u_Resolution);
-    vec4 final_colour = texture(u_PT_Texture, uv);
-    colour = vec4(final_colour.rgb, 1.0);
+    vec3 final_colour = texture(u_PT_Texture, uv).rgb;
+    colour = vec4(final_colour, 1.0);
 }
